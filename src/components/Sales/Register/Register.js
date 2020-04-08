4611fa9543386368becf88c2b3a8b73412521b5d
Tran Lam <lam.tran@miracles.vn>
2020-04-08 13:17:49 +0700

    Register Release-01

--- src/components/Sales/Register/Register.js
+++ src/components/Sales/Register/Register.js
@@ -2,8 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../../../assets/fontawesome/css/fontawesome.min.css';
 import '../../../assets/bootstrap.min.css';
-import Logo from '../../../assets/moon.png';
+import Logo from '../../../assets/images/logo.png';
 import Dropdown from 'react-dropdown';
+import Ava from '../../../assets/images/3489625.rsz.jpg';
 import 'react-dropdown/style.css';
 import './style.scss';
 
@@ -22,6 +23,7 @@ const Register = (props) => {
             controlClassName="register__dropdown"
             placeholderClassName="register__dropdown__item"
             arrowClassName="register__dropdown__arrow"
+            menuClassName="register__dropdown__menu"
             options={options}
             // onChange={this._onSelect}
             value={defaultOption}
@@ -29,21 +31,23 @@ const Register = (props) => {
           ></Dropdown>
         </div>
         <div className="register__group-btn">
-          <button className="register__screen-btn">
-            <i className="fa fa-arrows-alt" aria-hidden="true"></i>
+          <button className="register__logout-btn">
+            <i className="fas fa-expand-alt"></i>
           </button>
           <button className="register__logout-btn">
             <i className="fa fa-power-off" aria-hidden="true"></i>
-            Logout
+            Log out
           </button>
         </div>
       </div>
       <div className="register-main">
         <div className="register-main__wrap row">
-          <div className="register__account col-lg-4 col-xl-3">
+          <div className="register__account col-md-4  col-xl-3">
             <div className="register__account-card">
               <div className="register__account-b1">
-                <img className="register__account-img" src={Logo} alt=""></img>
+                <div className="register__account-img">
+                  <img src={Ava} alt=""></img>
+                </div>
                 <p>Dika Susanto</p>
               </div>
               <div className="register__account-b2">
@@ -71,10 +75,10 @@ const Register = (props) => {
               </div>
             </div>
           </div>
-          <div className="register__content col-lg-8 col-xl-9">
+          <div className="register__content col-md-8  col-xl-9">
             <div className="register__lits row">
               {options.map(() => (
-                <div className="register__items col-lg-4 col-xl-3">
+                <div className="register__items col-md-6 col-lg-4 col-xl-3">
                   <div className="register__items-wrap ">
                     <div className="register__items-top ">
                       <div className="register__items-right ">
